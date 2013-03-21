31d987a5cb02c5b5f559222609867fe7d0337835
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-03-21 23:23:26 +0100

    Some more documentation

--- src/Manager.h
+++ src/Manager.h
@@ -1,7 +1,7 @@
 /**
  * @file    Manager.h
  * @ingroup LoggerCpp
- * @brief   The static class that manage the registered channels and outputs
+ * @brief   The static class that manage the registered Channel and Output objects
  *
  * Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
  *
@@ -18,6 +18,13 @@
 
 /**
  * @brief The static class that manage the registered channels and outputs
+ *
+ *  The Manager keeps a map of all the named Channel objects
+ * and share them on demand by new Logger objects created with the same name.
+ *
+ * @todo Thus the Manager is able to change the Log::Level of selected Channel objectq.
+ *
+ * @todo The Manager also keep a list of all enabled Output object to output the Log objects.
  */
 struct Manager
 {
