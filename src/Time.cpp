556a152e085ee96634375afc45c1482520eb5bad
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-03-24 21:33:57 +0100

    Starting an Time utility class

---
+++ src/Time.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file    Time.cpp
+ * @ingroup LoggerCpp
+ * @brief   Current time precise to the millisecond.
+ *
+ * Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "Time.h"
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <time.h>
+#include <sys/time.h>
+#endif
+
+namespace Log
+{
+
+
+/// @todo Set to current time
+Time::Time(void)
+{
+#ifdef WIN32
+    SYSTEMTIME now;
+    GetLocalTime(&now);
+#else
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    struct tm* timeinfo = localtime(&now);
+#endif
+}
+
+
+} // namespace Log
---
+++ src/Time.h
@@ -0,0 +1,36 @@
+/**
+ * @file    Time.h
+ * @ingroup LoggerCpp
+ * @brief   Current time precise to the millisecond.
+ *
+ * Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#pragma once
+
+
+namespace Log
+{
+
+
+/**
+ * @brief   Current time precise to the millisecond.
+ * @ingroup LoggerCpp
+ */
+struct Time
+{
+    Time(void);
+
+    int ms;      /* milliseconds */
+    int sec;     /* seconds [0,59] */
+    int min;     /* minutes [0,59] */
+    int hour;    /* hours   [0,23] */
+    int day;     /* day     [1,31] */
+    int month;   /* month   [1,12] */
+    int year;    /* year    [0,30827]*/
+};
+
+
+} // namespace Log
