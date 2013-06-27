8c5624315f96ca156ce94a067c9616e8025a810f
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-06-27 07:11:26 +0200

    Updated minimal shared_ptr.hpp with recent fixes and cleanup

--- src/shared_ptr.hpp
+++ src/shared_ptr.hpp
@@ -55,6 +55,14 @@ namespace Log
 #else
 
 
+#include <cstddef>      // NULL
+#include <algorithm>    // std::swap
+
+// can be replaced by other error mechanism
+#include <cassert>
+#define SHARED_ASSERT(x)    assert(x)
+
+
 /**
  * @brief minimal implementation of smart pointer, a subset of the C++11 std::shared_ptr or boost::shared_ptr.
  *
@@ -66,67 +74,88 @@ template<class T>
 class shared_ptr
 {
 public:
-    /// @brief Default constructor
-    shared_ptr(void) throw() : // nothrow
+   typedef T element_type;
+
+   /// @brief Default constructor
+    shared_ptr(void) throw() : // never throws
         px(NULL),
         pn(NULL)
     {
     }
     /// @brief Constructor with the provided pointer to manage
-    explicit shared_ptr(T* p) : // throw std::bad_alloc
-        px(p),
+    explicit shared_ptr(T* p) : // may throw std::bad_alloc
+      //px(p), would be unsafe as acquire() may throw, which would call release() in destructor
         pn(NULL)
     {
-        acquire();
+        acquire(p);   // may throw std::bad_alloc
     }
-    /// @brief Copy constructor (used by the copy-and-swap idiom)
-    shared_ptr(const shared_ptr& ptr) : // throw std::bad_alloc
-        px(ptr.px),
+    /// @brief Constructor to share ownership. Warning : to be used for pointer_cast only ! (does not manage two separate <T> and <U> pointers)
+    template <class U>
+    shared_ptr(const shared_ptr<U>& ptr, T* p) :
+     //px(p), would be unsafe as acquire() may throw, which would call release() in destructor
+       pn(ptr.pn)
+    {
+       acquire(p);   // may throw std::bad_alloc
+    }
+    /// @brief Copy constructor to convert from another pointer type
+    template <class U>
+    shared_ptr(const shared_ptr<U>& ptr) throw() : // never throws (see comment below)
+      //px(ptr.px),
         pn(ptr.pn)
     {
-        acquire();
+        SHARED_ASSERT((NULL == ptr.px) || (NULL != ptr.pn)); // must be cohérent : no allocation allowed in this path
+        acquire(static_cast<typename shared_ptr<T>::element_type*>(ptr.px));   // will never throw std::bad_alloc
+    }
+    /// @brief Copy constructor (used by the copy-and-swap idiom)
+    shared_ptr(const shared_ptr& ptr) throw() : // never throws (see comment below)
+       //px(ptr.px),
+        pn(ptr.pn)
+    {
+        SHARED_ASSERT((NULL == ptr.px) || (NULL != ptr.pn)); // must be cohérent : no allocation allowed in this path
+        acquire(ptr.px);   // will never throw std::bad_alloc
     }
     /// @brief Assignment operator using the copy-and-swap idiom (copy constructor and swap method)
-    shared_ptr& operator=(shared_ptr ptr) throw() // nothrow
+    shared_ptr& operator=(shared_ptr ptr) throw() // never throws
     {
         swap(ptr);
         return *this;
     }
-    /// @brief the destructor release its ownership
-    inline ~shared_ptr(void) throw() // nothrow
+    /// @brief the destructor releases its ownership
+    inline ~shared_ptr(void) throw() // never throws
     {
         release();
     }
-    /// @brief this reset release its ownership
-    inline void reset(void) throw() // nothrow
+    /// @brief this reset releases its ownership
+    inline void reset(void) throw() // never throws
     {
         release();
     }
     /// @brief this reset release its ownership and re-acquire another one
-    void reset(T* p) throw() // nothrow
+    void reset(T* p) throw() // may throw std::bad_alloc
     {
+        SHARED_ASSERT((NULL == p) || (px != p)); // auto-reset not allowed
         release();
-        px = p;
-        acquire();
+        acquire(p); // may throw std::bad_alloc
     }
 
     /// @brief Swap method for the copy-and-swap idiom (copy constructor and swap method)
-    void swap(shared_ptr& lhs) throw() // nothrow
+    void swap(shared_ptr& lhs) throw() // never throws
     {
+        // Would be nice to enable use of ustl::swap by define
         std::swap(px, lhs.px);
         std::swap(pn, lhs.pn);
     }
 
     // reference counter operations :
-    inline operator bool() const throw() // nothrow
+    inline operator bool() const throw() // never throws
     {
         return (0 < use_count());
     }
-    inline bool unique(void)  const throw() // nothrow
+    inline bool unique(void)  const throw() // never throws
     {
         return (1 == use_count());
     }
-    long use_count(void)  const throw() // nothrow
+    long use_count(void)  const throw() // never throws
     {
         long count = 0;
         if (NULL != pn)
@@ -137,85 +166,38 @@ public:
     }
 
     // underlying pointer operations :
-    inline T& operator*()  const throw() // nothrow
+    inline T& operator*()  const throw() // never throws
     {
+        SHARED_ASSERT(NULL != px);
         return *px;
     }
-    inline T* operator->() const throw() // nothrow
+    inline T* operator->() const throw() // never throws
     {
+        SHARED_ASSERT(NULL != px);
         return px;
     }
-    inline T* get(void)  const throw() // nothrow
+    inline T* get(void)  const throw() // never throws
     {
+        // no assert, car return NULL
         return px;
-    }
-
-    // comparaison operators
-    inline bool operator== (const shared_ptr& ptr) const
-    {
-        return (px == ptr.px);
-    }
-    inline bool operator== (const T* p) const
-    {
-        return (px == p);
-    }
-    inline bool operator!= (const shared_ptr& ptr) const
-    {
-        return (px != ptr.px);
-    }
-    inline bool operator!= (const T* p) const
-    {
-        return (px != p);
-    }
-    inline bool operator<= (const shared_ptr& ptr) const
-    {
-        return (px <= ptr.px);
-    }
-    inline bool operator<= (const T* p) const
-    {
-        return (px <= p);
-    }
-    inline bool operator< (const shared_ptr& ptr) const
-    {
-        return (px < ptr.px);
-    }
-    inline bool operator< (const T* p) const
-    {
-        return (px < p);
-    }
-    inline bool operator>= (const shared_ptr& ptr) const
-    {
-        return (px >= ptr.px);
-    }
-    inline bool operator>= (const T* p) const
-    {
-        return (px >= p);
-    }
-    inline bool operator> (const shared_ptr& ptr) const
-    {
-        return (px > ptr.px);
-    }
-    inline bool operator> (const T* p) const
-    {
-        return (px > p);
     }
 
 private:
     /// @brief acquire/share the ownership of the px pointer, initializing the reference counter
-    void acquire(void)
+    void acquire(T* p) // may throw std::bad_alloc
     {
-        if (NULL != px)
+        if (NULL != p)
         {
             if (NULL == pn)
             {
                 try
                 {
-                    pn = new long(1);
+                    pn = new long(1); // may throw std::bad_alloc
                 }
                 catch (std::bad_alloc&)
                 {
-                    delete px;
-                    throw;
+                    delete p;
+                    throw; // rethrow the std::bad_alloc
                 }
             }
             else
@@ -223,10 +205,12 @@ private:
                 ++(*pn);
             }
         }
+        // here it is safe to acquire the ownership of the provided raw pointer, where exception cannot be thrown any more
+        px = p;
     }
 
     /// @brief release the ownership of the px pointer, destroying the object when appropriate
-    void release(void) throw() // nothrow
+    void release(void) throw() // never throws
     {
         if (NULL != pn)
         {
@@ -234,12 +218,17 @@ private:
             if (0 == *pn)
             {
                 delete px;
-                px = NULL;
                 delete pn;
-                pn = NULL;
             }
+            px = NULL;
+            pn = NULL;
         }
     }
+
+private:
+    // This allow pointer_cast functions to share the reference counter between different shared_ptr types
+    template<class U>
+    friend class shared_ptr;
 
 private:
     T*      px; //!< Native pointer
@@ -247,6 +236,54 @@ private:
 };
 
 
-#endif // LOGGER_USE_BOOST_SHARED_PTR
+// comparaison operators
+template<class T, class U> inline bool operator==(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() == r.get());
+}
+template<class T, class U> inline bool operator!=(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() != r.get());
+}
+template<class T, class U> inline bool operator<=(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() <= r.get());
+}
+template<class T, class U> inline bool operator<(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() < r.get());
+}
+template<class T, class U> inline bool operator>=(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() >= r.get());
+}
+template<class T, class U> inline bool operator>(const shared_ptr<T>& l, const shared_ptr<U>& r) throw() // never throws
+{
+    return (l.get() > r.get());
+}
+
+
+
+// static cast of shared_ptr
+template<class T, class U>
+shared_ptr<T> static_pointer_cast(const shared_ptr<U>& ptr) // never throws
+{
+    return shared_ptr<T>(ptr, static_cast<typename shared_ptr<T>::element_type*>(ptr.get()));
+}
+
+// dynamic cast of shared_ptr
+template<class T, class U>
+shared_ptr<T> dynamic_pointer_cast(const shared_ptr<U>& ptr) // never throws
+{
+    T* p = dynamic_cast<typename shared_ptr<T>::element_type*>(ptr.get());
+    if (NULL != p)
+    {
+        return shared_ptr<T>(ptr, p);
+    }
+    else
+    {
+        return shared_ptr<T>();
+    }
+}
 
 } // namespace Log
